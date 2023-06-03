f17c399db94cae0621d9898983151dd0e3b40a6a
marcusguiban <119875070+marcusguiban@users.noreply.github.com>
2023-06-03 15:42:30 +0800

    dentist create edit and view

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import HomePage from './pages/Utilities/homepage';
 import NotFound from './pages/Utilities/NotFound';
+
 import DentistList from './pages/Dentists/DentistsList';
+import DentistCreate from './pages/Dentists/DentistCreate';
+import DentistsView from './pages/Dentists/DentistView';
+import DentistEdit from './pages/Dentists/DentistEdit';
+
 function App() {
   return (
     <>
@@ -11,7 +16,11 @@ function App() {
     <Routes>
        <Route path="/" element={<HomePage />}></Route>
        <Route path="*" element={<NotFound />}></Route>
+
+       <Route path="/Dentists/edit/:id" element={<DentistEdit />}></Route>
+       <Route path="/Dentists/:id" element={<DentistsView />}></Route>
        <Route path="/Dentists" element={<DentistList />}></Route>
+       <Route path="/Dentists/new" element={<DentistCreate />}></Route>
     </Routes>
 </BrowserRouter>
     </>
